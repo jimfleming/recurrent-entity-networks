26dc9cb4e1c953206d5d61c2142f0267938e073b
Jim Fleming <jimmy.fleming@gmail.com>
2016-11-19 11:08:48 -0800

    Fix dynamic memory cell

--- babi_model/dynamic_memory_cell.py
+++ babi_model/dynamic_memory_cell.py
@@ -4,12 +4,21 @@ from __future__ import division
 
 import tensorflow as tf
 
+def batch_matmul_v_v(a, b):
+    """
+    Compute the dot product of two batched vectors.
+
+    a: [batch_size, d] or [d]
+    b: [batch_size, d] or [d]
+    returns: [batch_size]
+    """
+    return tf.reduce_sum(tf.mul(a, b), reduction_indices=[-1])
+
 class DynamicMemoryCell(tf.nn.rnn_cell.RNNCell):
 
-    def __init__(self, num_blocks, num_units_per_block, key_size, activation=tf.nn.relu):
+    def __init__(self, num_blocks, num_units_per_block, activation=tf.identity):
         self._num_blocks = num_blocks # M
         self._num_units_per_block = num_units_per_block # d
-        self._key_size = key_size # len(w_j)
         self._activation = activation # \phi
 
     @property
@@ -20,29 +29,41 @@ class DynamicMemoryCell(tf.nn.rnn_cell.RNNCell):
     def output_size(self):
         return self._num_blocks * self._num_units_per_block
 
+    def get_gate(self, inputs, state_j, key_j):
+        inputs_state_j = batch_matmul_v_v(inputs, state_j)
+        inputs_key_j = batch_matmul_v_v(inputs, key_j)
+        gate_j = tf.sigmoid(inputs_state_j + inputs_key_j)
+        gate_j = tf.expand_dims(gate_j, -1)
+        return gate_j
+
+    def get_candidate(self, inputs, W):
+        return self._activation(tf.matmul(inputs, W))
+
     def __call__(self, inputs, state, scope=None):
         state = tf.split(1, self._num_blocks, state)
-        input_size = inputs.get_shape().with_rank(2)[1] # 32 x 20
+        input_size = inputs.get_shape().with_rank(2)[1]
 
         # split into blocks (U, V, W are shared)
         with tf.variable_scope(scope or type(self).__name__):
-            state_next = []
+            next_states = []
             for j in range(self._num_blocks):
-                key_j = tf.get_variable('key_{}'.format(j), shape=[input_size, self._num_units_per_block]) # key vector (j): 20 x 100
-                state_j = state[j] # hidden state (j): ? x 100
+                state_j = state[j] # hidden state (j)
 
                 with tf.variable_scope('Gate'):
-                    gate_j = tf.sigmoid(tf.matmul(inputs, state_j) + tf.matmul(inputs, key_j)) # gate (j): ? x 100
+                    key_j = tf.get_variable('key_{}'.format(j),
+                        shape=[self._num_units_per_block]) # key vector (j)
+                    gate_j = self.get_gate(inputs, state_j, key_j) # 2) gate
 
                 reuse = False if j == 0 else True
                 with tf.variable_scope('Candidate', reuse=reuse):
-                    W = tf.get_variable('W', [input_size, self._num_units_per_block]) # 20 x 100
-                    candidate = self._activation(tf.matmul(inputs, W)) # ? x 100
+                    W = tf.get_variable('W',
+                        shape=[input_size, self._num_units_per_block])
+                    candidate = self.get_candidate(inputs, W) # 3) candidate
 
-                state_j_next = state_j + gate_j * candidate
-                state_j_next = tf.nn.l2_normalize(state_j_next, -1) # forget by normalization
+                state_j_next = state_j + gate_j * candidate # 4) update state
+                state_j_next = tf.nn.l2_normalize(state_j_next, -1) # 5) forget by normalization
 
-                state_next.append(state_j_next)
+                next_states.append(state_j_next)
 
-        state_next = tf.concat(1, state_next)
+        state_next = tf.concat(1, next_states)
         return state_next, state_next

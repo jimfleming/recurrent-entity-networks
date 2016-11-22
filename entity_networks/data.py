682ea8da297e3969ea8848678d3f85bb941a86c2
Jim Fleming <jimmy.fleming@gmail.com>
2016-11-21 20:53:40 -0800

    Shape sentences from data module

--- entity_networks/data.py
+++ entity_networks/data.py
@@ -21,19 +21,25 @@ def record_reader(filename_queue):
     story = sequence_features['story']
     query = sequence_features['query']
 
+    # TODO: Turn both functions into class methods with public properties
+    # to the story, query and answer as well as other vars.
     max_sentence_length = 7
     max_story_length = 10
     max_query_length = 4
 
     story.set_shape([max_story_length * max_sentence_length])
     query.set_shape([max_query_length])
+
+    story = tf.reshape(story, [max_story_length, max_sentence_length])
+    query = tf.reshape(query, [1, max_query_length])
+
     return story, query, answer
 
 def input_pipeline(filenames, batch_size, num_epochs=None, shuffle=False):
     filename_queue = tf.train.string_input_producer(filenames,
         num_epochs=num_epochs,
         shuffle=shuffle)
-    min_after_dequeue = 9000
+    min_after_dequeue = 10000
     capacity = min_after_dequeue + 10 * batch_size
     records = record_reader(filename_queue)
     batches = tf.train.shuffle_batch(records,

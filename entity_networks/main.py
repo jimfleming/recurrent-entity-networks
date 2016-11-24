7f4aaef9a2ee5d869d90eeb9b11509415021107a
Jim Fleming <jimmy.fleming@gmail.com>
2016-11-24 08:11:27 -0800

    Add run config and set seed

--- entity_networks/main.py
+++ entity_networks/main.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from __future__ import print_function
 from __future__ import division
 
+import os
 import time
 
 import random
@@ -11,7 +12,6 @@ import numpy as np
 np.random.seed(67)
 
 import tensorflow as tf
-tensorflow.set_random_seed(67)
 
 tf.logging.set_verbosity(tf.logging.INFO)
 
@@ -48,11 +48,20 @@ def main(_):
 
     eval_metrics = {"accuracy": tf.contrib.metrics.streaming_accuracy}
 
+    config = tf.contrib.learn.RunConfig(
+        tf_random_seed=67,
+        save_summary_steps=3,
+        save_checkpoints_secs=120,
+        keep_checkpoint_max=5,
+        keep_checkpoint_every_n_hours=1)
+
     dataset_name = os.path.splitext(os.path.basename(FLAGS.dataset))[0]
+    timestamp = int(time.time())
     logdir = 'logs/{}/{}/'.format(dataset_name, timestamp)
     estimator = tf.contrib.learn.Estimator(
         model_fn=model_fn,
         model_dir=logdir,
+        config=config,
         params=params)
 
     experiment = tf.contrib.learn.Experiment(

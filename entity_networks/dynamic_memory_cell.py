49ca6b7465ded93c6771f7b34e913d80b05a138c
Jim Fleming <jimmy.fleming@gmail.com>
2016-11-23 21:07:07 -0800

    Cleanup DMC

--- entity_networks/dynamic_memory_cell.py
+++ entity_networks/dynamic_memory_cell.py
@@ -11,10 +11,11 @@ class DynamicMemoryCell(tf.nn.rnn_cell.RNNCell):
     The cell's hidden state is divided into blocks and each block's weights are tied.
     """
 
-    def __init__(self, num_blocks, num_units_per_block, activation=tf.nn.relu):
+    def __init__(self, num_blocks, num_units_per_block, initializer=None, activation=tf.nn.relu):
         self._num_blocks = num_blocks # M
         self._num_units_per_block = num_units_per_block # d
         self._activation = activation # \phi
+        self._initializer = initializer
 
     @property
     def state_size(self):
@@ -30,9 +31,10 @@ class DynamicMemoryCell(tf.nn.rnn_cell.RNNCell):
 
         g_j <- \sigma(s_t^T h_j + s_t^T w_j)
         """
-        a = tf.reduce_sum(inputs * state_j, reduction_indices=[1])
-        b = tf.reduce_sum(inputs * key_j, reduction_indices=[1])
-        return tf.sigmoid(a + b)
+        with tf.variable_scope('Gate'):
+            a = tf.reduce_sum(inputs * state_j, reduction_indices=[1])
+            b = tf.reduce_sum(inputs * key_j, reduction_indices=[1])
+            return tf.sigmoid(a + b)
 
     def get_candidate(self, state_j, key_j, inputs, U, V, W):
         """
@@ -41,31 +43,24 @@ class DynamicMemoryCell(tf.nn.rnn_cell.RNNCell):
 
         h_j^~ <- \phi(U h_j + V w_j + W s_t)
         """
-        state_U = tf.matmul(state_j, U)
-        inputs_W = tf.matmul(inputs, W)
-        key_V = tf.matmul(tf.expand_dims(key_j, 0), V)
-        return self._activation(state_U + key_V + inputs_W)
+        with tf.variable_scope('Gate'):
+            state_U = tf.matmul(state_j, U)
+            inputs_W = tf.matmul(inputs, W)
+            key_V = tf.matmul(tf.expand_dims(key_j, 0), V)
+            return self._activation(state_U + key_V + inputs_W)
 
     def __call__(self, inputs, state, scope=None):
-        with tf.variable_scope(scope or type(self).__name__):
+        with tf.variable_scope(scope or type(self).__name__, initializer=self._initializer):
             # Split the hidden state into blocks (each U, V, W are shared across blocks).
-            state = tf.split(1, self._num_blocks, state) # TODO: This split/concat is relatively expensive.
+            state = tf.split(1, self._num_blocks, state) # TODO: split/concat is relatively expensive
 
-            U = tf.get_variable('U',
-                shape=[self._num_units_per_block, self._num_units_per_block],
-                initializer=tf.random_normal_initializer(0.1))
-            V = tf.get_variable('V',
-                shape=[self._num_units_per_block, self._num_units_per_block],
-                initializer=tf.random_normal_initializer(0.1))
-            W = tf.get_variable('W',
-                shape=[self._num_units_per_block, self._num_units_per_block],
-                initializer=tf.random_normal_initializer(0.1))
+            U = tf.get_variable('U', [self._num_units_per_block, self._num_units_per_block])
+            V = tf.get_variable('V', [self._num_units_per_block, self._num_units_per_block])
+            W = tf.get_variable('W', [self._num_units_per_block, self._num_units_per_block])
 
             next_states = []
             for j, state_j in enumerate(state): # Hidden State (j)
-                key_j = tf.get_variable('key_{}'.format(j),
-                    shape=[self._num_units_per_block],
-                    initializer=tf.random_normal_initializer(0.1))
+                key_j = tf.get_variable('key_{}'.format(j), [self._num_units_per_block])
                 gate_j = self.get_gate(inputs, state_j, key_j)
                 candidate_j = self.get_candidate(state_j, key_j, inputs, U, V, W)
 

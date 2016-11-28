0f24e70486b60e27535479c856f41b8ec18ee0f9
Jim Fleming <jimmy.fleming@gmail.com>
2016-11-27 22:05:41 -0800

    Remove alpha clipping as it did not help

--- entity_networks/activations.py
+++ entity_networks/activations.py
@@ -9,8 +9,7 @@ def prelu(features, initializer=None, scope=None):
     Implementation of [Parametric ReLU](https://arxiv.org/abs/1502.01852) borrowed from Keras.
     """
     with tf.variable_scope(scope, 'PReLU', initializer=initializer):
-        alpha = tf.get_variable('alpha',
-            shape=features.get_shape().as_list()[1:])
+        alpha = tf.get_variable('alpha', features.get_shape().as_list()[1:])
         pos = tf.nn.relu(features)
-        neg = tf.clip_by_value(alpha, 0, 1) * (features - tf.abs(features)) * 0.5 # TODO: Clipping may not be necessary
+        neg = alpha * (features - tf.abs(features)) * 0.5
         return pos + neg

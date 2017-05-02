124cb0ef135188ec8ccfffcf238d6898cf73abbf
Jim Fleming <jimmy.fleming@gmail.com>
2017-05-01 23:22:59 -0700

    Reverse and decrease early stopping

--- entity_networks/experiment.py
+++ entity_networks/experiment.py
@@ -48,7 +48,7 @@ def generate_experiment_fn(data_dir, dataset_id, num_epochs,
 
         run_config = tf.contrib.learn.RunConfig(
             save_summary_steps=train_steps_per_epoch,
-            save_checkpoints_steps=train_steps_per_epoch,
+            save_checkpoints_steps=10 * train_steps_per_epoch,
             save_checkpoints_secs=None)
 
         params = {

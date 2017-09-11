ce4e1b54758f4d5f354cccceebb7987082c373a4
Jim Fleming <jimmy.fleming@gmail.com>
2017-09-11 16:50:17 -0700

    Update download_babi.sh

--- download_babi.sh
+++ download_babi.sh
@@ -4,7 +4,7 @@ if [ ! -d ./data ]; then
   mkdir -p ./data
 fi
 
-BABI_TASKS=datasets/babi_tasks_data_1_20_v1.2.tar.gz
+BABI_TASKS=data/babi_tasks_data_1_20_v1.2.tar.gz
 
 if [ ! -f $BABI_TASKS ]; then
   wget http://www.thespermwhale.com/jaseweston/babi/tasks_1-20_v1-2.tar.gz -O $BABI_TASKS
